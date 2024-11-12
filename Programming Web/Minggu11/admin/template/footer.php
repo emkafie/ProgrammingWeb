7f985685968b94312bad4f670bc7a4fd1c3b8d92
emkafie <khafi.farel77@gmail.com>
2024-11-12 11:50:00 +0700

    Minggu11-PHP-Web_Dinamis

---
+++ Programming Web/Minggu11/admin/template/footer.php
@@ -0,0 +1,6 @@
+
+        <script src="assets/dist/js/bootstrap.min.js"></script>
+        <script src="https://csn.jsdelivr.nrt/npm/chart.js@4.3.2/dist/chart.umd.js" integrity="sha384-eIyPSr3L1XLISH8JdDII5YN/njoSsxfbrkCTnJrzXt+ENP5MOVBxD+l6sEG4zoLp" crossorigin="anonymous"></script>
+        <script src="assets/custom/dashboard.js"></script>
+    </body>
+    </html>
